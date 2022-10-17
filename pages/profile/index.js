bc2a4fca0743f03fbb596f20bb27505a8a3b29c3
Petrarca <pedropetrarcar@gmail.com>
2022-10-17 08:49:02 -0300

    savepoint 2

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,4 +1,19 @@
 const pullRequest = localStorage.getItem("profileSearched")
 const helloAgain = JSON.parse(pullRequest)
+console.log(helloAgain.avatar_url,helloAgain.name, helloAgain.bio)
 
-console.log(helloAgain)
+const profileCard = document.querySelector(".profile-card")
+const profileFigure = document.querySelector(".profile-figure")
+const profileInfos = document.querySelector(".profile-infos")
+
+function headerAssembly(userData) {
+    const profileImg = document.createElement('Img')
+    const profileName = document.createElement('h3')
+    const profileStack = document.createElement('p')
+
+    profileImg.src = userData.avatar_url
+    profileName.innerText = userData.name
+    
+
+}
+headerAssembly(helloAgain)
