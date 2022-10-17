f884b044422201f49f268f75d7366305a0fa37c9
Petrarca <pedropetrarcar@gmail.com>
2022-10-17 10:53:32 -0300

    savepoint 5

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,11 +1,11 @@
 const pullRequest = localStorage.getItem("profileSearched")
 const helloAgain = JSON.parse(pullRequest)
-console.log(helloAgain.avatar_url,helloAgain.name, helloAgain.company)
+
 
 const profileCard = document.querySelector(".profile-card")
 const profileFigure = document.querySelector(".profile-figure")
 const profileInfos = document.querySelector(".profile-infos")
-
+const repositoriesFeed = document.querySelector(".projects-feed")
 
 function headerAssembly(userData) {
     const profileImg = document.createElement('Img')
@@ -14,9 +14,36 @@ function headerAssembly(userData) {
 
     profileImg.src = userData.avatar_url
     profileName.innerText = userData.name
-    profileStack.innerText = userData.company
+    profileStack.innerText = userData.bio
 
     profileFigure.append(profileImg)
     profileInfos.append(profileName, profileStack)
 }
 headerAssembly(helloAgain)
+
+fetch(helloAgain.repos_url)
+    .then(function (response) {
+        return response.json()
+    })
+    .then((responseJson) => {
+        responseJson.map((repository) => {
+        const repoCard = document.createElement('div')
+        const name = document.createElement('h4')
+        const repoDescriptionbox = document.createElement('div')
+        const description = document.createElement('p')
+        const repoButtonsBox = document.createElement('div')
+        const openBtn = document.createElement('button')
+        const demoBtn = document.createElement('button')
+
+        name.innerText = repository.name
+        description.innerText = repository.full_name
+
+
+        repoDescriptionbox.append(description)
+        repoButtonsBox.append(openBtn, demoBtn)
+        repoCard.append(name, repoDescriptionbox, repoButtonsBox)
+        repositoriesFeed.append(repoCard)
+    })
+    })
+
+    
